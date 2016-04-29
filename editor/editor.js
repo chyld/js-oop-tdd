2cee3992d37ae0a0b3d48474f7ba6f0bcbd7b6ea
Chyld Medford <chyld.medford@gmail.com>
2016-04-29 21:19:15 +0000

    editor

--- editor/editor.js
+++ editor/editor.js
@@ -1,1 +1,46 @@
-// your code here
+function Editor(){
+  this.actions = [];
+  this.current = -1;
+}
+
+Editor.prototype.write = function(str){
+  this.current += 1;
+  this.actions[this.current] = {
+    type: 'write',
+    data: str
+  };
+};
+
+Editor.prototype.replace = function(old, neu){
+  this.current += 1;
+  this.actions[this.current] = {
+    type: 'replace',
+    data: {old: old, neu: neu}
+  };
+};
+
+Editor.prototype.undo = function(str){
+  this.current -= 1;
+};
+
+Editor.prototype.redo = function(str){
+  this.current += 1;
+};
+
+Editor.prototype.toString = function(){
+  var cache = '';
+
+  for(var i = 0; i <= this.current; i++){
+    var action = this.actions[i];
+    if(action.type === 'write'){
+      cache += action.data;
+    }else{
+      var regex = new RegExp(action.data.old, 'g');
+      cache = cache.replace(regex, action.data.neu);
+    }
+  }
+
+  return cache;
+};
+
+module.exports = Editor;
--- editor/spec/editor_spec.js
+++ editor/spec/editor_spec.js
@@ -1,97 +1,77 @@
-// var Editor = require('../editor')
+var Editor = require('../editor')
 
-// describe('Editor', function () {
+describe('Editor', function () {
 
-//   it("allows users to write text", function () {
-//     var editor = new Editor()
-//     editor.write("Hello - codez")
-//     expect(editor.toString()).toEqual("Hello - codez")
+  it("allows users to write text", function () {
+    var editor = new Editor()
+    editor.write("Hello - codez")
+    expect(editor.toString()).toEqual("Hello - codez")
 
-//     editor.write(" moar")
-//     expect(editor.toString()).toEqual("Hello - codez moar")
-//   })
+    editor.write(" moar")
+    expect(editor.toString()).toEqual("Hello - codez moar")
+  })
 
-//   // it("allows users to undo writes", function () {
-//   //   var editor = new Editor()
-//   //   editor.write("Hello - codez")
-//   //   expect(editor.toString()).toEqual("Hello - codez")
-//   //   editor.write("Moar stuff")
-//   //   expect(editor.toString()).toEqual("Hello - codezMoar stuff")
-//   //   editor.write("Even more")
-//   //   expect(editor.toString()).toEqual("Hello - codezMoar stuffEven more")
-//   //
-//   //   editor.undo()
-//   //   expect(editor.toString()).toEqual("Hello - codezMoar stuff")
-//   //
-//   //   editor.undo()
-//   //   expect(editor.toString()).toEqual("Hello - codez")
-//   //
-//   //   editor.undo()
-//   //   expect(editor.toString()).toEqual("")
-//   // })
-//   //
-//   // it("allows users to find and replace", function () {
-//   //   var editor = new Editor()
-//   //   editor.write("foo stuff")
-//   //   editor.write(" other foo")
-//   //
-//   //   editor.replace("foo", "bar")
-//   //   expect(editor.toString()).toEqual("bar stuff other bar")
-//   // })
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   // it("allows undo replaces", function () {
-//   //   var editor = new Editor()
-//   //   editor.write("foo stuff")
-//   //   editor.write(" other foo")
-//   //
-//   //   editor.replace("foo", "bar")
-//   //   expect(editor.toString()).toEqual("bar stuff other bar")
-//   //
-//   //   editor.undo()
-//   //   expect(editor.toString()).toEqual("foo stuff other foo")
-//   //
-//   //   editor.undo()
-//   //   expect(editor.toString()).toEqual("foo stuff")
-//   // })
-//   //
-//   // it("allows users to redo", function () {
-//   //   var editor = new Editor()
-//   //   editor.write("foo stuff")
-//   //   editor.write(" other foo")
-//   //   editor.replace("foo", "bar")
-//   //   expect(editor.toString()).toEqual("bar stuff other bar")
-//   //
-//   //   editor.undo()
-//   //   expect(editor.toString()).toEqual("foo stuff other foo")
-//   //
-//   //   editor.undo()
-//   //   expect(editor.toString()).toEqual("foo stuff")
-//   //
-//   //   editor.redo()
-//   //   expect(editor.toString()).toEqual("foo stuff other foo")
-//   //
-//   //   editor.redo()
-//   //   expect(editor.toString()).toEqual("bar stuff other bar")
-//   // })
+  it("allows users to undo writes", function () {
+    var editor = new Editor()
+    editor.write("Hello - codez")
+    expect(editor.toString()).toEqual("Hello - codez")
+    editor.write("Moar stuff")
+    expect(editor.toString()).toEqual("Hello - codezMoar stuff")
+    editor.write("Even more")
+    expect(editor.toString()).toEqual("Hello - codezMoar stuffEven more")
 
-// })
+    editor.undo()
+    expect(editor.toString()).toEqual("Hello - codezMoar stuff")
+
+    editor.undo()
+    expect(editor.toString()).toEqual("Hello - codez")
+
+    editor.undo()
+    expect(editor.toString()).toEqual("")
+  })
+
+  it("allows users to find and replace", function () {
+    var editor = new Editor()
+    editor.write("foo stuff")
+    editor.write(" other foo")
+
+    editor.replace("foo", "bar")
+    expect(editor.toString()).toEqual("bar stuff other bar")
+  })
+
+  it("allows undo replaces", function () {
+    var editor = new Editor()
+    editor.write("foo stuff")
+    editor.write(" other foo")
+
+    editor.replace("foo", "bar")
+    expect(editor.toString()).toEqual("bar stuff other bar")
+
+    editor.undo()
+    expect(editor.toString()).toEqual("foo stuff other foo")
+
+    editor.undo()
+    expect(editor.toString()).toEqual("foo stuff")
+  })
+
+  it("allows users to redo", function () {
+    var editor = new Editor()
+    editor.write("foo stuff")
+    editor.write(" other foo")
+    editor.replace("foo", "bar")
+    expect(editor.toString()).toEqual("bar stuff other bar")
+
+    editor.undo()
+    expect(editor.toString()).toEqual("foo stuff other foo")
+
+    editor.undo()
+    expect(editor.toString()).toEqual("foo stuff")
+
+    editor.redo()
+    expect(editor.toString()).toEqual("foo stuff other foo")
+
+    editor.redo()
+    expect(editor.toString()).toEqual("bar stuff other bar")
+  })
+
+})

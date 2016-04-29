70fbe967499073b25321ef93711cdbce9150a999
Chyld Medford <chyld.medford@gmail.com>
2016-04-29 19:23:13 +0000

    car.js

--- cars/car.js
+++ cars/car.js
@@ -1,7 +1,28 @@
-var Car = function (mpg) {
+var Car = function(mpg){
+  this.mpg = mpg;
+  this.gallons = 0;
+  this.miles = 0;
+  this._trips = [];
 };
 
-Car.prototype.fill = function (gallons) {
+Car.prototype.fill = function(gallons){
+  this.gallons += gallons;
+};
+
+Car.prototype.drive = function(miles){
+  this.miles += miles;
+  this.gallons -= (miles / this.mpg);
+  this._trips.push(miles);
+};
+
+Car.prototype.odometer = function(){
+  return this.miles;
+};
+
+Car.prototype.trips = function(){
+  return this._trips.map(function(t){
+    return t + ' miles';
+  });
 };
 
 module.exports = Car;

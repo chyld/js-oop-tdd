b6fd35c92096759de2f910b01ed1ffd9f1ee4a31
Chyld Medford <chyld.medford@gmail.com>
2016-04-29 20:08:45 +0000

    dmv

--- dmv/dmv.js
+++ dmv/dmv.js
@@ -1,7 +1,56 @@
-var DMV = function (agents) {
+function Queue(){
+  this.items = [];
+}
+
+Queue.prototype.enqueue = function(item){
+  this.items.unshift(item);
 };
 
-DMV.prototype.customersInLine = function () {
+Queue.prototype.dequeue = function(){
+  return this.items.pop();
+};
+
+// ------------------------------------------- //
+// ------------------------------------------- //
+// ------------------------------------------- //
+
+var DMV = function(agents){
+  this.active = {};
+  this.agents = new Queue();
+  this.customers = new Queue();
+  agents.forEach(agent => {
+    this.agents.enqueue(agent);
+  });
+};
+
+DMV.prototype.customersInLine = function(){
+  return this.customers.items.slice().reverse();
+};
+
+DMV.prototype.enter = function(customer){
+  return this.customers.enqueue(customer);
+};
+
+DMV.prototype.currentCustomerFor = function(agent){
+  return this.active[agent] || null;
+};
+
+DMV.prototype.nextCustomer = function(){
+  var customer = this.customers.dequeue();
+  var agent = this.agents.dequeue();
+  this.active[agent] = customer;
+};
+
+DMV.prototype.resolve = function(customer){
+  var agent;
+  for(var prop in this.active){
+    if(this.active[prop] === customer){
+      agent = prop;
+      break;
+    }
+  }
+  this.agents.enqueue(agent);
+  this.active[agent] = null;
 };
 
 module.exports = DMV;
